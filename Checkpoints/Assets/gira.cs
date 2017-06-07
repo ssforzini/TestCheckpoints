10863fb4a63c31e608842c4d1ab6a5d411cfc726
ssforzini <sebasforzini@hotmail.com>
2017-06-06 21:56:28 -0300

    Proyecto de prueba para variables de sistema

---
+++ Checkpoints/Assets/gira.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class gira : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (Vector3.up);
+
+		if(Input.anyKeyDown){
+			SceneManager.LoadScene ("scene");
+		}
+		//posdiff = objetouno.position - objetotarget.position; OBTIENE EL VECTOR DE DISTANCIA ENTRE 2 OBJETOS.
+		//DontDestroyOnLoad(gameobject); Mantiene un objeto entre escenas
+	}
+}
---
+++ Checkpoints/Assets/movement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class movement : MonoBehaviour {
+
+	void Start(){
+		if (PlayerPrefs.HasKey ("PositionX")) {
+			transform.position = new Vector3 (PlayerPrefs.GetFloat ("PositionX"),PlayerPrefs.GetFloat ("PositionY"),PlayerPrefs.GetFloat ("PositionZ"));
+		} else {
+			transform.position = new Vector3 (-1f,1f,8.5f);
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		if(Input.GetAxis ("Horizontal") != 0){
+			float mov = Input.GetAxis ("Horizontal");
+			transform.Translate (Vector3.forward * Time.deltaTime * mov * 3f);
+		}
+
+	}
+
+	void OnTriggerEnter(Collider col){
+		PlayerPrefs.SetFloat ("PositionX", -1f);
+		PlayerPrefs.SetFloat ("PositionY", 1f);
+		PlayerPrefs.SetFloat ("PositionZ", col.transform.position.z - 0.5f);
+		if(col.name == "fourCheck"){
+			PlayerPrefs.SetFloat ("PositionZ", 8.5f);
+			SceneManager.LoadScene ("scene2");
+		}
+	}
+}
